a6615b92bbb965fd08016de649ce8faf672e9665
Alonzo Lee <zoodfv@gmail.com>
2022-07-29 00:06:25 -0500

    Initial Commit

---
+++ courses/serializers.py
@@ -0,0 +1,8 @@
+from pyexpat import model
+from rest_framework import serializers
+from .models import Course
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+  class Meta:
+    model = Course
+    fields = ('id', 'url', 'name', 'language', 'price')
